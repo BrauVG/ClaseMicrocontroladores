f81e82ff616a018f2487f1311764b1ad95cbc918
Braulio Vargas <72416515+BrauVG@users.noreply.github.com>
2025-06-03 10:52:20 -0600

    Add files via upload
    Contador de 0 a F haciendo uso de la matriz 8x8

---
+++ 3_1Matriz8x8_0aF/Matriz8x8.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Matriz8x8.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Matriz8x8.X.production.hex
