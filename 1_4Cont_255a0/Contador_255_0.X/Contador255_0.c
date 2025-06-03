e4e152ffcdbebaf7ee0da803bedacb126b6f5d83
Braulio Vargas <72416515+BrauVG@users.noreply.github.com>
2025-06-03 01:42:55 -0600

    Add files via upload
    Contador binario del 255 a 0 representados por led

---
+++ 1_4Cont_255a0/Contador_255_0.X/Contador255_0.c
@@ -0,0 +1,28 @@
+/*
+ * Intituto Politecnico Nacional
+ * Escuela Superior de Ingenieria Mecanica y Electrica
+ * Ingenieria en comunicaciones y electronica
+ * Microcontroladores
+ * 
+ * File:   Contador255_0.c
+ * Author: Braulio Alberto Vargas Garcia
+ * Co-Author: Aldo Flores Vega
+ *
+ * Created on 1 de febrero de 2023, 11:49 PM
+ */
+
+#define F_CPU 1000000UL
+#include <XC.h>
+#include <util/delay.h>
+
+int main(void) {
+    DDRB=255;
+    while (1) 
+    {
+        for(int i=0;i<=255;i++)
+        {
+            PORTB=~i;
+            _delay_ms(250);
+        }
+    }
+}
